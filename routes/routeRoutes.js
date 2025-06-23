729001baa34915a83852a984269fad6c38234caf
Jeff <jeff@abc.com>
2025-06-23 10:39:34 +0700

    new code

--- routes/routeRoutes.js
+++ routes/routeRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const routeController = require('../controllers/routeControllers');
 const { authenticateToken } = require('../middleware/auth');
 
+// Add middleware to all routes
 router.post('/', authenticateToken, routeController.createRoute);
 router.get('/nearby', routeController.getNearbyRoutes);
 router.get('/nearby-both', authenticateToken, routeController.getRoutesNearbyBothLocations);
